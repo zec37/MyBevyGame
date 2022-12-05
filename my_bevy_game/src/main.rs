b2d0797297596ef7bfdb590c42de178cbd522796
zec37 <zhaiguanxun@hotmail.com>
2022-12-05 18:31:35 +0800

    - 3d_scene practice

--- my_bevy_game/src/main.rs
+++ my_bevy_game/src/main.rs
@@ -1,42 +1,75 @@
 use bevy::prelude::*;
 
-pub struct HelloPlugin;
+//////////////////////////////// Get Started.
+// pub struct HelloPlugin;
 
-#[derive(Resource)]
-struct GreetTimer(Timer);
+// #[derive(Resource)]
+// struct GreetTimer(Timer);
 
-#[derive(Component)]
-struct Person;
+// #[derive(Component)]
+// struct Person;
 
-#[derive(Component)]
-struct Name(String);
+// #[derive(Component)]
+// struct Name(String);
 
-fn add_people(mut commands: Commands) {
-    commands.spawn((Person, Name("Elaina Proctor".to_string())));
-    commands.spawn((Person, Name("Renzo Hume".to_string())));
-    commands.spawn((Person, Name("Zayna Nieves".to_string())));
-}
+// fn add_people(mut commands: Commands) {
+//     commands.spawn((Person, Name("Elaina Proctor".to_string())));
+//     commands.spawn((Person, Name("Renzo Hume".to_string())));
+//     commands.spawn((Person, Name("Zayna Nieves".to_string())));
+// }
 
-fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
-    if timer.0.tick(time.delta()).just_finished(){
-        for name in query.iter() {
-            println!("hello {}!", name.0);
-        }
-    }
-}
+// fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+//     if timer.0.tick(time.delta()).just_finished(){
+//         for name in query.iter() {
+//             println!("hello {}!", name.0);
+//         }
+//     }
+// }
 
-impl Plugin for HelloPlugin {
-    fn build(&self, app: &mut App) {
-        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
-            .add_startup_system(add_people)
-            .add_system(greet_people);
-    }
-}
+// impl Plugin for HelloPlugin {
+//     fn build(&self, app: &mut App) {
+//         app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+//             .add_startup_system(add_people)
+//             .add_system(greet_people);
+//     }
+// }
 
 fn main() {
     
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugin(HelloPlugin)
+        .add_startup_system(setup)
         .run();
+}
+
+//////////////////////////////// 3d_scene
+fn setup(
+    mut commands:Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Plane {size: 5.0})),
+        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+        ..default()
+    });
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube {size: 1.0})),
+        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+        transform: Transform::from_xyz(0.0, 0.5, 0.0),
+        ..default()
+    });
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            intensity: 1500.0, 
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..default()
+    });
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
 }
