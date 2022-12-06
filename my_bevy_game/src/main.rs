b9143d979f13e44e612bd0b84c25ab04843e02ef
zec37 <zhaiguanxun@hotmail.com>
2022-12-06 21:29:16 +0800

    Isolate files

--- my_bevy_game/src/main.rs
+++ my_bevy_game/src/main.rs
@@ -4,9 +4,5 @@ use bevy::prelude::*;
 fn main() {
     
     App::new()
-        .add_plugins(DefaultPlugins)
-        .add_startup_system(setup)
-        .add_system(movement)
-        .add_system(animate_light_direction)
-        .run();
+        .add_plugins(DefaultPlugins);
 }
